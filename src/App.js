8c3862d1230414e7e12989998053a3a02aad7874
akgraham <99699836+akgraham@users.noreply.github.com>
2022-10-10 07:59:53 -0600

    Imported Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-       This project was coded by <a href="https://www.shecodes.io/graduates/34653-anolani-graham" rel="noreferrer" target="_blank">Anolani Graham</a> and is {""}
-       <a href="https://github.com/akgraham/react-weather-app" rel="noreferrer" target="_blank">
-        open-sourced on GitHub
-       </a>
-      </footer>
+      <div className="container">
+       <h1>Weather App</h1>
+       <footer>
+         This project was coded by <a href="https://www.shecodes.io/graduates/34653-anolani-graham" rel="noreferrer" target="_blank">Anolani Graham</a> and is {""}
+         <a href="https://github.com/akgraham/react-weather-app" rel="noreferrer" target="_blank">
+         open-sourced on GitHub
+        </a>
+       </footer>
+      </div>
     </div>
   );
 }
